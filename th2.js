cb271b3d83499493b7dfa7810f09e6f9c65c0d79
Bình <vanbinhhd1996@gmail.com>
2021-06-09 13:42:48 +0700

    bai toan boi so

---
+++ th2.js
@@ -0,0 +1,14 @@
+let inputWidth;
+let inputHeigth;
+inputWidth = prompt('nhap chieu rong');
+inputHeigth = prompt('nhap chieu dai');
+let width = parseInt( inputWidth);
+let height = parseInt( inputHeigth);
+let area = width * height ;
+document.write('width: =' +width);
+document.write('<br/>');
+document.write('heigth: =' +height);
+document.write('<br/>');
+document.write('dien tich: =' +area);
+
+
