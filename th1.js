0903248b16d3e4598c4a76928bfc4cf0b7286bc2
Bình <vanbinhhd1996@gmail.com>
2021-06-09 11:31:02 +0700

    tai len bai 7

---
+++ th1.js
@@ -0,0 +1,32 @@
+// //bai tap 1;
+// let i = 10;
+// let f = 20.5;
+// let b = true;
+// let s = "ha noi";
+// document.write('i =' + i );
+// document.write('<br/>');
+// document.write('f = ' + f );
+// document.write('<br/>');
+// document.write('b = ' + b);
+// document.write('<br/>');
+// document.write('s = ' + s);
+
+//bai tap 2;
+// let width = 20;
+// let height = 10;
+// let area = width * height;
+// document.write('-width =' + width);
+// document.write('<br/>');
+// document.write('-height =' + height)
+// document.write( '</br>');
+// document.write('=>area =' +area);
+
+// // bai tap 3;
+// a = prompt('nhap a');
+// b = prompt( ' nhap b');
+// if (a%b==0){
+//     alert('a chia het cho b');
+// }else{
+//     alert('a khong chia het cho b');
+// }
+
